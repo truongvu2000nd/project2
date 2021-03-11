efe6d61e6e57e20fd677fba52c181cd1edebf64b
truongvu2000nd <truongvu0911nd@gmail.com>
2021-03-11 18:04:58 +0700

    create model song

--- backend/music/models.py
+++ backend/music/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    file = models.FileField(upload_to='musics/')
+
+    def __str__(self):
+        return self.title
--- backend/music/views.py
+++ backend/music/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import SongSerializer
+from .models import Song
 
 # Create your views here.
+class SongView(viewsets.ModelViewSet):
+    serializer_class = SongSerializer
+    queryset = Song.objects.all()
